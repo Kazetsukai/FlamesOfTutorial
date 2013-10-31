9e59524848c9561b3e4b18edb17089fb6d0d079e
Daniel Tebbutt <moeruze.burning+git@gmail.com>
2013-10-31 22:00:32 +1300

    Added version control safe unity project

---
+++ FlamesOfTutorial/Assets/ScriptController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ScriptController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            foreach (GameObject obj in FindObjectsOfType(typeof(GameObject)))
+            {
+                obj.SendMessage("Deselect", SendMessageOptions.DontRequireReceiver);
+            }
+
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                var tank = hit.transform.gameObject;
+                var script = tank.GetComponent(typeof(ISelectable)) as ISelectable;
+
+                if (script != null)
+                {
+                    script.Select();
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                foreach (GameObject obj in FindObjectsOfType(typeof(GameObject)))
+                {
+                    obj.SendMessage("Move", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+	}
+}
---
+++ FlamesOfTutorial/Assets/TankingScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankingScript : MonoBehaviour, ISelectable, IMovable {
+
+    private bool _selected = false;
+	
+    // Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (_selected)
+            renderer.material.color = Color.red;
+        else
+            renderer.material.color = Color.white;
+	}
+
+    public void Select()
+    {
+        _selected = true;
+    }
+
+    public void Deselect()
+    {
+        _selected = false;
+    }
+
+    public void Move(Vector3 destination)
+    {
+        rigidbody.AddForce((destination - transform.position + Vector3.up * 10) * 20);
+        Debug.Log("Move!");
+    }
+}
